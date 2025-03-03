bce6a5d296a4b137d2a69faf4807ec67c0fc27ee
Mohammad Tasin <tasinkamal78@gmail.com>
2025-03-03 23:38:11 +0600

    Create 1537B - Bad Boy.cpp

---
+++ 1537B - Bad Boy.cpp
@@ -0,0 +1,50 @@
+/* In this problem the two yo yo should be in corner connection. So we have to show two co ordinates where Anton will be . so we take antons 
+  positon as a co ordinate , then also printed 1 1 which can be a co ordinate for all cases */
+
+
+//    Assalamu’alaikum, warahmatullahi wabarakatuh   //
+          //    BISMILLAH HIR RAHMAN NIR RAHIM   //
+          //    “Above all is Allah the King, Truth Before the revelation of the Qur’an upon you is complete.
+          //    Don’t be in a hurry with him; rather, my God, advance me in knowledge.”  //
+          //    *** AUTHOR : MOHAMMAD TASIN ***    //
+#include <bits/stdc++.h>
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+#define ll long long
+#define yes "YES\n"
+#define no "NO\n"
+#define all(s) s.begin(), s.end()
+using namespace std;
+ 
+void inputOutput()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+void solve(){
+ 
+        int t;   cin>>t;
+        while(t--)
+        {
+            ll n,m,i,j;  cin>>n>>m>>i>>j;
+ 
+            cout<<"1 " <<"1 "<<n<<" "<<m<<endl;
+ 
+        }
+ 
+}
+ 
+ 
+int  main(){
+ 
+    inputOutput();
+    solve();
+    return 0;
+ 
+}
